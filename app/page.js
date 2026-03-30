73a7fa92885ccbc5fec997738740a3a30a095028
Santiago <santifama@gmail.com>
2026-03-30 20:49:50 -0300

    redirect raiz al panel

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'sans-serif', color: '#999' }}>
-      404
-    </div>
-  )
+  redirect(`/panel/${process.env.NEXT_PUBLIC_SECRET_PANEL_PATH}`)
 }
